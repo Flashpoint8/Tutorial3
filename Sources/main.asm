4a3739e1cb965ecfd3c1179f581561e23f384269
Flashpoint8 <flashpoint_infinity@hotmail.com>
2016-01-31 19:30:29 -0500

    TemplateFile

--- Sources/main.asm
+++ Sources/main.asm
@@ -2,19 +2,15 @@
 ;*********************************************************************               
 ;*                      2DP4 Microcontrollers                        *
 ;*                            Tutorial 3                             *
-;*                           Jan 25, 2016                            *
+;*                           Jan 31, 2016                            *
 ;*********************************************************************
 ;*********************************************************************
-;*                       Tutorial 3 Companion Code                         
+;*                       Tutorial 4 Companion Code                         
 ; BEFORE YOU START!!!!!!!!!!!!! READ THIS!!!!!!!!!!!!!!!!
 ;
-; Read Tutorial 3
-; This code will access memory that will have random numbers (since this code does not assign any value to them)
-; Make sure you check each memroy location (select memory window, select window and address) to verify the value was copied over
-; Only look at the HNZVC bits in the CCR. 
-; This codes uses Register X which we have not discussed in tutorial yet.
+; Read Tutorial 4
 ;
-; Follow along using Tutorial 3 to trace the code as it executes                                                          
+; Follow along using Tutorial 4 to trace the code as it executes                                                          
 ;*********************************************************************
 ;*********************************************************************
 ;*                            References                             
@@ -22,7 +18,6 @@
 ;* HCS12/9S12 Textbook                                                                   
 ;*                                                                   
 ;*********************************************************************
-
 ;*********************************************************************
 ;*                   Setup Code (Don't Change)                       
 ;*********************************************************************
@@ -33,7 +28,7 @@
 ; Include derivative-specific definitions 
 		        INCLUDE 'derivative.inc' 
 		        
-ROMStart    EQU  $0400  ; absolute address to place my code/constant data
+ROMStart    EQU  $1500  ; absolute address to place my code/constant data
 
 ; variable/data section
 
@@ -80,47 +75,7 @@ Entry
 
 ; Tutorial 3 Code 
 			
-      ;Inherent Mode II: No operand (T3 p19)
-            NOP ;
-            
-      ;Inherent Mode II: All operand are internal CPU register (T3 p19)
-            INCA ;  LOOK THIS UP IN THE INSTRUCTION SET! What does it do? What mode? What is the machine code?
       
-      ;Immediate Mode: Operand IS the number (T3 p20)
-            LDAA #$20       ;hex number, notice proceeding #
-            LDAA #%00100001 ;binary number, notice proceeding #
-            
-      ;Direct Mode: Address is only 8 bits long (T3 p21-22) 
-            LDAA $20 ; notice nothing in front of number. In this case whatever is loaded in $20 will be copied to Accumulator A
-            
-      ;Extended Mode: Address is 16 bits long (T3 p21-22)
-            LDAA $1600; notice nothing in front of number. In this case whatever is loaded in $1600 will be copied to Accumulator A           
-
-      ;Indexed Addressing: Constant Offset (T3 p23-27)
-            LDX #$1600 ; this uses register x which we have not discussed in tutorial. We will!
-            LDAA 2,X
-            
-      ;Indexed Addressing: Offset in Accumulator (T3 p23-27)
-            LDAB #$04
-            LDAA B,X
-
-      ;Assembler Directive (T3 p29-30)
-            ;WAI; UNCOMMENT TO USE. notice that this will not take any space in memory since it is an instruction to the assembler
-            
-      ;CCR (T3 p41-42)
-            LDAA #$79
-            LDAB #$34
-            
-            SBA ; LOOK THIS UP IN THE INSTRUCTION SET! What does it do? What mode? What CCR bits should be effected? Check only HNZVC
-            
-      ;CCR (T3 p41-42)
-            LDAA #$34
-            LDAB #$79
-            
-            SBA ; LOOK THIS UP IN THE INSTRUCTION SET! What does it do? What mode? What CCR bits should be effected? Check only HNZVC                  
-     
-            WAI
-
 ;*********************************************************************
 ;*                Interrupt Vectors (Don't Change)                   *
 ;*********************************************************************
