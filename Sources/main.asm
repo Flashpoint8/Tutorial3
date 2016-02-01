d463773453aff5aaa2ed4e06e7473cf314ca2d4d
Flashpoint8 <flashpoint_infinity@hotmail.com>
2016-01-31 19:59:14 -0500

    Jan 31 Final Version
    All code updated to implement tutorial

--- Sources/main.asm
+++ Sources/main.asm
@@ -1,7 +1,7 @@
 ;*                       McMaster University                         *
 ;*********************************************************************               
 ;*                      2DP4 Microcontrollers                        *
-;*                            Tutorial 3                             *
+;*                            Tutorial 4                             *
 ;*                           Jan 31, 2016                            *
 ;*********************************************************************
 ;*********************************************************************
@@ -9,13 +9,18 @@
 ; BEFORE YOU START!!!!!!!!!!!!! READ THIS!!!!!!!!!!!!!!!!
 ;
 ; Read Tutorial 4
+; The memory cells used will not correspond to the tutorial
+; This is due to setup overhead and lighting the heartbeat LED
+; This code does not contain any I/O beyond heatbeat LED
+; Challenge 3/4 handle input/ output on the Esduino
 ;
 ; Follow along using Tutorial 4 to trace the code as it executes                                                          
 ;*********************************************************************
 ;*********************************************************************
 ;*                            References                             
-;* Tutorial 3                                                                  
-;* HCS12/9S12 Textbook                                                                   
+;* Tutorial 4                                                              
+;* HCS12/9S12 Textbook               
+;* Instruction Set                                                    
 ;*                                                                   
 ;*********************************************************************
 ;*********************************************************************
@@ -57,11 +62,11 @@ Entry
 ;*********************************************************************
 ; BEFORE YOU START!!!!!!!!!!!!! READ THIS!!!!!!!!!!!!!!!!
 ;
-; Read Tutorial 3
-; This code will access memory that will have random numbers (since this code does not assign any value to them)
-; Make sure you check each memroy location (select memory window, select memory from menubar and address) to verify the value was copied over
+; Read Tutorial 4
+; This code is plug and play (all values are hardcoded)
+; Have the instruction set open and look up all the instructions. How many clock cycles does each line need?
 ; Only look at the HNZVC bits in the CCR. 
-; This codes uses Register X which we have not discussed in tutorial yet. 
+; This codes uses Register X/Y which we have not discussed in tutorial yet. 
 ;*********************************************************************
 
 ; Your code will be stored starting at address $0400 (you can change it at line 31)
@@ -82,38 +87,41 @@ BRANCH LDAA #$02
        SUBA #$01
        TBEQ A, BRANCH
        LDAA #$9
-       WAI
+       ;We can't have WAI here since there is more code to follow	
 
 ; Tutorial 4 Page 41
+; WARNING: THIS WILL STICK YOU IN AN INFINITE LOOP!
+; YOU WILL NEED TO COMMENT OUT THIS SECTION TO CONTINUE WITH THE TUTORIAL 
+
 BRANCHS LDAA #$01
         SUBA #$01
         TBEQ A, BRANCHS
-        LDAA #$9
-        WAI		
+        LDAA #$9  ; This branch will never load Accumulator A with the value 9!
+        
         
 ; Tutorial 4 Page 50
-           LDY #10
+     LDY #10 ; Notice it appears as A in the register- why?
 
-LOOPOFF_IN NOP 
-           NOP
-           NOP
-           NOP
-           NOP
-           DBNE Y, LOOPOFF_IN
+LOOP NOP 
+     NOP
+     NOP
+     NOP
+     NOP
+     DBNE Y, LOOP
 
 ; Tutorial 4 Page 53
             LDY #2    ; set Y=10 as our outer loop
 LOOPOFF_OUT LDX #2 ;set X=50000 as our inner loop
-            LOOPOFF_IN  NOP
-                        NOP
-                        NOP
-                        NOP
-                        NOP 
-		                    DBNE x,LOOPOFF_IN	     ;decrement x, if x is not zero go to label         
-		                    DBNE y,LOOPOFF_OUT    ;decrement y, if y is not zero go to label 
+LOOPOFF_IN  NOP
+            NOP
+            NOP
+            NOP
+            NOP 
+		        DBNE x,LOOPOFF_IN	     ;decrement x, if x is not zero go to label         
+		        DBNE y,LOOPOFF_OUT    ;decrement y, if y is not zero go to label 
            
            
-	
+	          WAI 
       
 ;*********************************************************************
 ;*                Interrupt Vectors (Don't Change)                   *
